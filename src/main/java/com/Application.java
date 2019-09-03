8038204f6da79b03598e32cfe7731c0360e8e04c
瑞祥 马 <425002448@qq.com>
2019-09-03 13:03:20 +0800

    reduce unused package

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -3,9 +3,6 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @MapperScan("com.sm.mapper") //扫描的mapper
